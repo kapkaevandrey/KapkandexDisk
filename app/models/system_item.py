e35cef6aa196c014db0c4b31518edc77222771f9
Kapkaev Andrey <kapkaew@yandex.ru>
2022-09-10 18:19:55 +0300

    Refactoring

--- app/models/system_item.py
+++ app/models/system_item.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from sqlalchemy import Column, ForeignKey, CheckConstraint, String
+from sqlalchemy import (
+    Column, DateTime, ForeignKey, CheckConstraint, String,
+)
 from sqlalchemy.orm import relationship
 
 from app.models import SystemItemBase
@@ -12,10 +14,22 @@ class SystemItem(SystemItemBase):
         CheckConstraint('size > 0', 'size_must_be_positive')
     )
 
-    id = Column(String, primary_key=True)
-    parent_id = Column(String, ForeignKey('systemitem.id'), nullable=True)
-    children = relationship('SystemItem', cascade='all,delete')
-    history = relationship('SystemItemHistory', cascade='all,delete')
+    id = Column(
+        String, primary_key=True
+    )
+    parent_id = Column(
+        String, ForeignKey('systemitem.id'),
+        nullable=True
+    )
+    children = relationship(
+        'SystemItem',
+        cascade='all,delete', order_by='SystemItem.date'
+    )
+    history = relationship(
+        'SystemItemHistory',
+        cascade='all,delete', order_by='SystemItemHistory.date'
+    )
+    date = Column(DateTime(timezone=True))
 
     def __repr__(self):
         return (f'Type - "{self.type}" '
