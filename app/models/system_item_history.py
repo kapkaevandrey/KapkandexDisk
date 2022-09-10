190371f5c4def5ea630a9e31374ebf052a9656eb
Kapkaev Andrey <kapkaew@yandex.ru>
2022-09-10 18:20:25 +0300

    Refactoring

--- app/models/system_item_history.py
+++ app/models/system_item_history.py
@@ -1,15 +1,28 @@
 from app.models import SystemItemBase
 
 from sqlalchemy import (
-    Column, ForeignKey,
+    Column, DateTime, ForeignKey,
     BigInteger, String
 )
 
 
 class SystemItemHistory(SystemItemBase):
-    id = Column(BigInteger, primary_key=True)
-    item_id = Column(ForeignKey('systemitem.id'), nullable=False)
-    parent_id = Column(String, nullable=True)
+    id = Column(
+        BigInteger,
+        primary_key=True
+    )
+    item_id = Column(
+        ForeignKey('systemitem.id'),
+        nullable=False, index=True
+    )
+    parent_id = Column(
+        String,
+        nullable=True
+    )
+    date = Column(
+        DateTime(timezone=True),
+        index=True
+    )
 
     def __repr__(self):
         return (
