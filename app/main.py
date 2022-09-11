8aa7eb3dcaa083760222bda7cd22a7c567576b82
Kapkaev Andrey <kapkaew@yandex.ru>
2022-09-11 18:57:19 +0300

    add exception handlers  for custom message.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.exceptions import ValidationError, HTTPException, RequestValidationError
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 from app.api.routers import main_router
 from app.core.config import settings
@@ -9,3 +14,29 @@ app = FastAPI(
     description=settings.app_description
 )
 app.include_router(main_router)
+
+
+@app.exception_handler(HTTPException)
+async def validation_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=HTTPStatus.NOT_FOUND,
+        content=jsonable_encoder(
+            dict(
+                code=HTTPStatus.NOT_FOUND,
+                message='Item not found',
+            )
+        )
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=HTTPStatus.BAD_REQUEST,
+        content=jsonable_encoder(
+            dict(
+                code=HTTPStatus.BAD_REQUEST,
+                message='Validation Failed',
+            )
+        )
+    )
