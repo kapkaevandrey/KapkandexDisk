8b98b53ca03dde3ae15846fca1755c415ced4c27
Kapkaev Andrey <kapkaew@yandex.ru>
2022-09-12 01:21:30 +0300

    add validators check_date_is valid unique package_id and check the category unchanged.

--- app/api/validators.py
+++ app/api/validators.py
@@ -2,13 +2,15 @@ from collections import defaultdict
 from datetime import datetime
 from dateutil import tz, parser
 from http import HTTPStatus
-from typing import Any, List
+from typing import Any, List, Optional
 
 from fastapi import HTTPException
 from fastapi.exceptions import RequestValidationError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.crud.base import BaseCRUD, ModelType
+from app.schemas.system_item import SystemItemCreate
+from app.models import SystemItem
 
 
 async def try_get_object_by_attribute(
@@ -36,9 +38,38 @@ def parse_and_check_date(date: str) -> datetime:
         date = parser.isoparse(date)
     except ValueError:
         raise RequestValidationError('Invalid date format')
-    if date > datetime.now(tz.tzutc()):
+    check_date_is_valid(date)
+    return date
+
+
+def check_date_is_valid(
+        date: datetime, lower_date=Optional[datetime]
+) -> None:
+    if lower_date is None:
+        lower_date = datetime.now(tz.tzutc())
+    if date > lower_date:
         raise RequestValidationError(
             f'date in UTC cant be larger then '
-            f'{datetime.now(tz.tzutc())}'
+            f'{lower_date}'
         )
-    return date
+
+
+def check_items_package_unique_id(
+        items: List[SystemItemCreate],
+) -> None:
+    uniq_id_counter = defaultdict(int)
+    for item in items:
+        uniq_id_counter[item.id] += 1
+        if uniq_id_counter[item.id] > 1:
+            raise RequestValidationError(
+                    f'Items have not unique ids equal id={item.id} '
+            )
+
+
+def check_category_unchanged(item: SystemItemCreate, item_obj: SystemItem):
+    if item.type != item_obj.type:
+        raise RequestValidationError('You cannot change type of Item')
+
+
+
+
